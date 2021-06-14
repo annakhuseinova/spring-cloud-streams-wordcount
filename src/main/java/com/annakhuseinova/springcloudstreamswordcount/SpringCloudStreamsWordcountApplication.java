ce44ca8e3f19e9048b954d0aad667b21ad41b849
annakhuseinova <annakhuseinova@yandex.ru>
2021-06-14 15:48:48 +0300

    Initial commit

---
+++ src/main/java/com/annakhuseinova/springcloudstreamswordcount/SpringCloudStreamsWordcountApplication.java
@@ -0,0 +1,13 @@
+package com.annakhuseinova.springcloudstreamswordcount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudStreamsWordcountApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudStreamsWordcountApplication.class, args);
+    }
+
+}
